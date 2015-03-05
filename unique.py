c0205fd7ae78cd856c3a51244430fbee547ea410
Ned Williamson <nedwilliamson@gmail.com>
2015-03-05 15:55:57 -0500

    refactor, move files as necessary

--- unique.py
+++ unique.py
@@ -3,7 +3,6 @@
 
 #qira needs to be installed with BAP. also you need to run
 #source venv/bin/activate and eval $(opam config env)
-
 import sys
 sys.path.append("middleware/")
 import time
@@ -11,10 +10,9 @@ import os
 import qira_program, qira_analysis, qira_config
 from glob import glob
 import gc
-
-if len(sys.argv) != 2:
-    print "usage: {} [directory]".format(sys.argv[0])
-    sys.exit()
+import traceback
+import argparse
+from subprocess import call
 
 #based on get_instruction_flow from qira_analysis
 #arm flag tells us whether to disasm or not, autodetect from ELF?
@@ -82,8 +80,6 @@ def read_asm_file(self):
       inst = d[d.find(":")+3:]
     cnt += 1
 
-fn = sys.argv[1]
-
 #from my static testing code
 ######
 class bcolors(object):
@@ -116,19 +112,9 @@ def get_file_list(loc, recursive=True):
   return fns
 ######
 
-file_list = get_file_list(sys.argv[1])
-
 from elftools.common.exceptions import ELFError
 
-d = {}
-for i,fn in enumerate(file_list):
-  short_fn = fn.split("/")[-1]
-  print "{} [{}/{}] files initally processed...".format(star_blue, i+1, len(file_list))
-  try:
-    program = qira_program.Program(fn)
-  except ELFError:
-    print "{} skipping non-ELF `{}'...".format(warn, short_fn)
-    continue
+def process_fn(program):
   program.clear()
   program.execqira()
   time.sleep(.1) # we have to wait for the trace :/
@@ -139,23 +125,9 @@ for i,fn in enumerate(file_list):
   #while not trace.db.did_update(): #is this better? from qira_analysis.py
   #  time.sleep(0.1)
   program.qira_asm_file = open("/tmp/qira_asm", "r")
-  read_asm_file(program)
-  d[short_fn] = get_unique_instructions(trace, program)
-  del trace    #remove references to trace and program, then gc
-  del program  #this way we don't OOM if we don't have to
-  gc.collect()
-#print d
-
-#put all elements in a start set
-#while entries remain:
-#pick entry with largest set
-#  insert it
-#  do set difference on all remaining sets, removing those that become empty
-
-d_orig = d
-#weird unpacking here. can probably do better
-all_elements = set.union(*[v for (_,v) in d_orig.iteritems()])
-#use this to check that everything is hit at the end
+  read_asm_file(program) #so we get thumb flag if arm
+  instructions = get_unique_instructions(trace, program)
+  return instructions
 
 def get_largest_name(d):
   assert len(d) > 0
@@ -166,29 +138,78 @@ def get_largest_name(d):
     if current_uniq_ins > current_max_uniq_ins:
       current_max_fn = fn
       current_max_uniq_ins = current_uniq_ins
-  assert type(current_max_fn) is str #we set the fn name
+  assert type(current_max_fn) is str #we set the fn name, not None
   return current_max_fn
 
-min_set = set()
-while len(d) > 0:
-  print "{} {} files remaining in min_set selection...".format(star_blue, len(d))
-  largest_name = get_largest_name(d)
-  min_set.add(largest_name)
-  largest_elements = d[largest_name]
-  d_new = {}
-  for k,v in d.iteritems():
-    if k == largest_name:
-      continue
-    v_new = v.difference(largest_elements)
-    if len(v_new) > 0:
-      d_new[k] = v_new
-  d = d_new
+def get_unique_instruction_dict(file_list):
+    d = {}
+    failed = set()
+    for i,fn in enumerate(file_list):
+      short_fn = fn.split("/")[-1]
+      print "{} [{}/{}] files initally processed...".format(star_blue, i+1, len(file_list))
+      try:
+        program = qira_program.Program(fn)
+      except ELFError:
+        print "{} skipping non-ELF `{}'...".format(warn, short_fn)
+        continue
+      try:
+        d[fn] = process_fn(program)
+      except Exception as exn:
+        print "{} processing {} failed".format(fail, short_fn), type(exn).__name__, exn
+        print traceback.format_exc()
+        failed.add(fn)
+    return d, failed
 
-#check that we have all the instructions in the min_set
-new_all_elements = set.union(*[d_orig[k] for k in min_set])
-assert all_elements == new_all_elements
+def get_min_set(folder_name):
+    file_list = get_file_list(folder_name)
+    d, failed = get_unique_instruction_dict(file_list)
+    if len(d) == 0:
+        print "No instructions executed."
+        sys.exit()
+    d_orig = d
+    #weird unpacking here. can probably do better
+    all_elements = set.union(*[v for (_,v) in d_orig.iteritems()])
+    #use this to check that everything is hit at the end
 
-print "min_set identified!"
-print "original input",d_orig.keys()
-print "minimized input",min_set
-print "{} There was a total reduction from {} input files to {} input files.".format(ok_green, len(d_orig), len(min_set))
+    min_set = set()
+    while len(d) > 0:
+      print "{} {} files remaining in min_set selection...".format(star_blue, len(d))
+      largest_name = get_largest_name(d)
+      min_set.add(largest_name)
+      largest_elements = d[largest_name]
+      d_new = {}
+      for k,v in d.iteritems():
+        if k == largest_name:
+          continue
+        v_new = v.difference(largest_elements)
+        if len(v_new) > 0:
+          d_new[k] = v_new
+      d = d_new
+
+    #check that we have all the instructions in the min_set
+    new_all_elements = set.union(*[d_orig[k] for k in min_set])
+    assert all_elements == new_all_elements
+
+    print "{} There was a total reduction from {} input files to {} output files.".format(ok_green, len(d_orig), len(min_set))
+    if len(failed) > 0:
+        print "{} An error occurred when processing the following {} files:".format(warn, len(failed)),failed
+    return (min_set, failed)
+
+def move_files(min_set, failed, dest_folder):
+    call(["mkdir", "-p", dest_folder])
+    print "move files called"
+    for fn in min_set | failed:
+        print "moving",fn
+        short_fn = fn.split("/")[-1]
+        dest = os.path.join(dest_folder,short_fn)
+        #print " ".join(["cp", fn, dest])
+        call(["cp", fn, dest])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Minimize test corpus to cover all instructions.")
+    parser.add_argument("input_folder", help="input folder")
+    parser.add_argument("output_folder", help="output folder")
+    args = parser.parse_args()
+    print args.input_folder, args.output_folder
+    min_set, failed = get_min_set(args.input_folder)
+    move_files(min_set, failed, args.output_folder)
